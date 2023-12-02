f6e3021bf3a13248001031f25c376f045b895b09
Rampawara99 <ramjadhaw1999@gmail.com>
2023-12-02 22:38:28 +0530

    responsiveness added

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -4,6 +4,27 @@ import { Link } from 'react-router-dom';
 
 const Nav = () => {
 
+    // nav menu handler
+    const openHandler = () => {
+        const menu = document.querySelector('.menu-list');
+        const open = document.querySelector('.open');
+        const close = document.querySelector('.close');
+        menu.style.top = '90%';
+        open.classList.toggle('d_block');
+        close.classList.toggle('d_block');
+    }
+
+
+    // close buttons
+    const closeHandler = () => {
+        const menu = document.querySelector('.menu-list');
+        const open = document.querySelector('.open');
+        const close = document.querySelector('.close');
+        menu.style.top = '-300%';
+        open.classList.toggle('d_block');
+        close.classList.toggle('d_block');
+    }
+
     return (
         <header className="navbar" >
             <div className="navbar-container">
@@ -12,10 +33,22 @@ const Nav = () => {
                 </div>
                 <div className="menu-container">
                     <ul className="menu-list">
-                        <li className="menu-list-item"> <Link to='/' className='active'>Home</Link></li>
-                        <li className="menu-list-item"><Link to='/'>Classes</Link></li> 
+                        <li className="menu-list-item"> <Link to='/' className='bold'>Home</Link></li>
+                        <li className="menu-list-item"><Link to='/'>Classes</Link></li>
                     </ul>
                 </div>
+            </div>
+            <div>
+                <span
+                    className="material-symbols-outlined close"
+                    onClick={closeHandler}>
+                    close
+                </span>
+                <span
+                    className="material-symbols-outlined open d_block"
+                    onClick={openHandler}>
+                    menu
+                </span>
             </div>
         </header>
     )
